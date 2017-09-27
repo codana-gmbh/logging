f4d095bcbf73f3c47f2dbf7ef9b3b897640e9ad6
Nicolas Huber <nico@hubaers.de>
2017-09-27 11:10:12 +0200

    Added static creation method for console sink

--- src/sinks/console/ConsoleSink.ts
+++ src/sinks/console/ConsoleSink.ts
@@ -1,10 +1,16 @@
 import {ILogEventSink} from "../../core/ILogEventSink";
 import {LogEvent} from "../../events/LogEvent";
 import {ITextFormatter} from "../../formatting/ITextFormatter";
+import {LogEventTokenizer} from "../../rendering/LogEventTokenizer";
+import {TokenRenderer} from "../../rendering/TokenRenderer";
 import {ConsoleFormatter} from "./ConsoleFormatter";
 import {ConsoleTextBuffer} from "./ConsoleTextBuffer";
 
 export class ConsoleSink implements ILogEventSink {
+    public static create(): ConsoleSink {
+        return new ConsoleSink(new ConsoleFormatter(new LogEventTokenizer(), new TokenRenderer()));
+    }
+
     private _textFormatter: ITextFormatter;
 
     public constructor(textFormatter: ConsoleFormatter) {
