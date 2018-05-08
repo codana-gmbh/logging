7ea81c4574001b86141d5a51489783a8baa80899
Nicolas Huber <nico@hubaers.de>
2018-05-08 10:49:09 +0200

    made error handling more solid and added undefined check for undefined or null values in value formatter

--- src/formatting/LogEventPropertyValueFormatter.ts
+++ src/formatting/LogEventPropertyValueFormatter.ts
@@ -1,15 +1,16 @@
 export class LogEventPropertyValueFormatter {
-
-	public format<TValue>(value: TValue): string {
-		const valueType = typeof value;
-		switch (valueType) {
-			case "string":
-				return value.toString();
-			case "object":
-				return JSON.stringify(value, null, 2);
-			default:
-				return value.toString();
-		}
-	}
-
+  public format<TValue>(value: TValue): string {
+    if (value == undefined) {
+      return "undefined";
+    }
+    const valueType = typeof value;
+    switch (valueType) {
+      case "string":
+        return value.toString();
+      case "object":
+        return JSON.stringify(value, null, 2);
+      default:
+        return value.toString();
+    }
+  }
 }
--- src/sinks/SafeAggregateSink.ts
+++ src/sinks/SafeAggregateSink.ts
@@ -1,23 +1,28 @@
-import {ILogEventSink} from "../core/ILogEventSink";
-import {LogEvent} from "../events/LogEvent";
+import { ILogEventSink } from "../core";
+import { LogEvent } from "../events";
 
 export class SafeAggregateSink implements ILogEventSink {
-    private readonly _sinks: ILogEventSink[];
+  private readonly _sinks: ILogEventSink[];
 
-    public constructor(sinks: ILogEventSink[]) {
-        this._sinks = sinks;
-    }
+  public constructor(sinks: ILogEventSink[]) {
+    this._sinks = sinks;
+  }
 
-    public emit(logEvent: LogEvent): void {
-        this._sinks.forEach((x) => {
-            try {
-                x.emit(logEvent);
-            }
-            catch (error) {
-                /* tslint:disable:no-console */
-                console.error("An error occured while emitting log event to sink", error);
-                /* tslint:enable:no-console */
-            }
-        });
-    }
+  public emit(logEvent: LogEvent): void {
+    this._sinks.forEach((x) => {
+      try {
+        x.emit(logEvent);
+      } catch (error) {
+        /* tslint:disable:no-console */
+        if (console != undefined) {
+          if (console.error != undefined) {
+            console.error("An error occured while emitting log event to sink");
+          } else if (console.log != undefined) {
+            console.log("An error occured while emitting log event to sink");
+          }
+        }
+        /* tslint:enable:no-console */
+      }
+    });
+  }
 }
